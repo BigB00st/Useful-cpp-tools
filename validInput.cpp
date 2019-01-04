f9600ec14cbcefd465e49b6b2be0d37833af0544
Amit Botzer <botzer.2002@gmail.com>
2019-01-04 11:39:57 +0200

    added printAndGetInput function

--- validInput.cpp
+++ validInput.cpp
@@ -1,16 +1,20 @@
+//Functions use iostream
+
 /*F+F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F
   Function: validInput
 
   Summary:  Function gets input from user. Cleans buffer and removes
-  endline. Returns true if input was the expected input type.
+			endline. Returns true if input was the expected input type.
 
-  Args:    template<class>
+  Args:    <typename> input
 			  Variable to input into.
 
   Returns:  bool
 			  True if input was the expected input type, false if not.
+
+  Note: typename must be input-able by std::cin
 F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F-F*/
-template<class T>
+template<typename T>
 bool validInput(T & input)
 {
 	bool valid = false;
@@ -22,6 +26,40 @@ bool validInput(T & input)
 	std::cin.ignore(INT_MAX, '\n');
 
 	return valid;
+}
+
+
+/*F+F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F
+  Function: validInput
+
+  Summary:  Function clears screen, prints given message, gets 
+			valid input from user, returns it.
+
+  Args:    const char * msg
+			  Message to print each loop
+		   <typename> min
+			  Minimum acceptable input
+		   <typename> max
+			  Maximum acceptable input
+
+  Returns:  <typename> choice
+			  The choice of the user regrading the printed message
+  
+  Note:  typename must support operator: <=
+F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F-F*/
+template<typename T>
+T printAndGetInput(const char * msg, const T & min, const T & max)
+{
+	T choice = 0;
+	bool valid = false;
+	do
+	{
+		system("cls");
+		std::cout << msg;
+		valid = validInput(choice);
+	} while (!valid || !(min <= choice <= max));
+
+	return choice;
 }
 
 /**--------------------------------------------------------
